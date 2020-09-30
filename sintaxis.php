b168d07f94c3c742a29c551ba2f725a49196e95a
Robert Hernandez Arenas <52069820+dimelorobert@users.noreply.github.com>
2020-09-30 10:11:06 +0200

    Conociendo la sintaxis PHP

---
+++ sintaxis.php
@@ -0,0 +1,7 @@
+<?php
+# PRINT imprime un solo resultado
+print "<h1>Hello world from PHP!</h1>";
+# ECHO imprime varios resultados, puedo añadir tag HTML
+echo "<p>Mi primer codigo</p>", "Mi segundo codigo";
+
+?>
